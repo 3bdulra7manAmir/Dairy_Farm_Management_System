4ef22b9c6297f324353571ae224a54fe92c8a801
Abdulrahman <ethanhunt4d@gmail.com>
2023-02-26 14:06:47 +0200

    Adjusting Breeing Page Size & Color & CloseBox and Center screen_AbdulrahmanAmir

--- Breeing.Designer.cs
+++ Breeing.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Breeing
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1280, 800);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Breeing";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Breeing";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
