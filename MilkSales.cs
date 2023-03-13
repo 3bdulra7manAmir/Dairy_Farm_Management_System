cc9b879ec27171c8c31d52cc6699238cb7b6720c
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-13 16:07:27 +0200

    Calling FillEmpId in MilkSales Page _Abdulrahman Amir

--- MilkSales.cs
+++ MilkSales.cs
@@ -16,6 +16,7 @@ namespace Dairy_Farm_Management_System
         public MilkSales()
         {
             InitializeComponent();
+            FillEmpId();
         }
 
         SqlConnection COn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""D:\Projects\Visual Studio 2022\Dairy Farm Management System\DairyFarmManagementSystem.mdf"";Integrated Security=True;Connect Timeout=30");
