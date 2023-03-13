07eec74311591fafbc95dedff2a5438136161cec
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-13 20:30:41 +0200

    Fxing Error in Finance Page _AbdulrahmanAmir

--- Finance.cs
+++ Finance.cs
@@ -16,7 +16,7 @@ namespace Dairy_Farm_Management_System
         public Finance()
         {
             InitializeComponent();
-            populate();
+            populateExp();
         }
 
         private void label4_Click(object sender, EventArgs e)
