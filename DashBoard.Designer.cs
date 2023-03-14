bbea398dfbbe33a15eb874d74bf27d33d33498c6
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-14 15:38:38 +0200

    Changing ForeColor to white in finance label in DashBoard Page _AbdulrahmanAmir

--- DashBoard.Designer.cs
+++ DashBoard.Designer.cs
@@ -397,7 +397,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(119, 13);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(149, 40);
