3680fcdba574cadd1480e79873ec19b344ac6c52
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-14 15:37:14 +0200

    Changing label finance bg color to Transparent in DashBoard Page _AbdulrahmanAmir

--- DashBoard.Designer.cs
+++ DashBoard.Designer.cs
@@ -395,7 +395,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.DimGray;
             this.label2.Location = new System.Drawing.Point(119, 13);
