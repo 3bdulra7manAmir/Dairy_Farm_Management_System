a60e47c12b04219bf5a4a686e64fc51663a40d1a
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-15 17:32:31 +0200

    Creating Logistic Function in DashBoard Page _AbdulrahmanAmir

--- DashBoard.cs
+++ DashBoard.cs
@@ -104,6 +104,28 @@ namespace Dairy_Farm_Management_System
             COn.Close();
         }
 
+        private void Logistic()
+        {
+            //
+            COn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select Count(*) from CowTbl", COn);
+            //SqlDataAdapter sda1 = new SqlDataAdapter("select sum(ExpAmount) from ExpenditrueTbl", COn);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            CowNumLbl.Text = dt.Rows[0][0].ToString();
+            //int inc, exp;
+            //double bal;
+            //inc = Convert.ToInt32(dt.Rows[0][0].ToString());
+            //IncLbl.Text = "Rs" + dt.Rows[0][0].ToString();
+            /*DataTable dt1 = new DataTable();
+            sda1.Fill(dt1);
+            exp = Convert.ToInt32(dt1.Rows[0][0].ToString());
+            bal = inc - exp;
+            ExpLbl.Text = "Rs" + dt1.Rows[0][0].ToString();
+            BalLbl.Text = "Rs" + bal;*/
+            COn.Close();
+        }
+
         private void DashBoard_Load(object sender, EventArgs e)
         {
 
