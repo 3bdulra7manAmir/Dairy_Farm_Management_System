91b351820d94bfb44f33d84414c6eb9b9681d909
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-15 16:24:54 +0200

    changing Logisticks color to MeduimVioletRed in DashBoard Page _AbdulrahmanAmir

--- DashBoard.Designer.cs
+++ DashBoard.Designer.cs
@@ -510,7 +510,7 @@
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label9.ForeColor = System.Drawing.Color.MediumVioletRed;
             this.label9.Location = new System.Drawing.Point(169, 7);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(164, 40);
