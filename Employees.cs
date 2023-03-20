5c07c78d9cdc39ca44d303f7c8e40115d45be0ce
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-20 16:04:30 +0200

    finishing Employess Savebtn code _AbdulrahmanAmir

--- Employees.cs
+++ Employees.cs
@@ -55,7 +55,7 @@ namespace Dairy_Farm_Management_System
                 try
                 {
                     COn.Open();
-                    string Query = "insert into EmployeeTbl values ('" + EmployeesNameTb.Text + "','" + DOB.Value.Date +  "','" + GenCb.SelectedItem.ToString() + "','"  +  PhoneTb.Text + "','" + AddressTb.Text + "')";
+                    string Query = "insert into EmployeeTbl values ('" + EmployeesNameTb.Text + "','" + DOB.Value.Date +  "','" + GenCb.SelectedItem.ToString() + "','"  +  PhoneTb.Text + "','" + AddressTb.Text + "'," + EmpPassTb.Text+"')";
                     SqlCommand cmd = new SqlCommand(Query, COn);
                     cmd.ExecuteNonQuery();
                     MessageBox.Show("Employee Saved Successfully");
