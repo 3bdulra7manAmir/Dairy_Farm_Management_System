4b37b44650a88609736f2793874d72190c0e07d0
Abdulrahman <ethanhunt4d@gmail.com>
2023-03-20 15:46:17 +0200

    Adding data base Connection in Login Page_ Abdulrahman Amir

--- Login.cs
+++ Login.cs
@@ -25,7 +25,25 @@ namespace Dairy_Farm_Management_System
 
         private void button1_Click(object sender, EventArgs e)
         {
+            if(RoleCb.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecte a Role");
+            }
+            if(PasswordTb.Text == "" || UnameTb.Text == "")
+            {
+                MessageBox.Show("Enter Admin Name and Password");
+            }
             if(RoleCb.SelectedItem.ToString() == "Admin")
+            {
+                Employees emp = new Employees();
+                emp.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Wrong Admin name and Password");
+            }
+            if(RoleCb.SelectedItem.ToString() == "Employee")
             {
 
             }
